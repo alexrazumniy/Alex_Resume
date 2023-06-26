2c42568d8be96c714cd3149f801bd73ed97cf73c
= <=>
2023-06-26 22:15:21 +0300

    contacts and skills added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./styles/main.css";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import ContactInfo from "./components/ContactInfo";
+import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 // import Loader from "./components/Loader";
 
 const App = () => {
@@ -8,6 +11,9 @@ const App = () => {
     <div className="container">
       <Header />
       <AboutMe />
+      <ContactInfo />
+      <Experience />
+      <Skills />
       {/* <Loader /> */}
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,9 @@
-import telegram_icon from "../assets/social_icons/telegram.png";
-import viber_icon from "../assets/social_icons/viber.png";
-import whatsapp_icon from "../assets/social_icons/whatsapp.png";
-import linkedin_icon from "../assets/social_icons/linkedin.png";
-import github_logo from "../assets/social_icons/github_logo.png";
 import { ThemeContext, themes } from "../context/ThemeContext";
 import Toggle from "../components/ThemeToggler";
 
 const Header = () => {
   return (
     <div className="header">
-      <nav>
-        <a className="social_link">
-          <img
-            className="social_icon"
-            src={telegram_icon}
-            alt={telegram_icon}
-          />
-        </a>
-        <a className="social_link">
-          <img className="social_icon" src={viber_icon} alt={viber_icon} />
-        </a>
-        <a className="social_link">
-          <img
-            className="social_icon"
-            src={whatsapp_icon}
-            alt={whatsapp_icon}
-          />
-        </a>
-        <a className="social_link">
-          <img
-            className="social_icon"
-            src={linkedin_icon}
-            alt={linkedin_icon}
-          />
-        </a>
-        <a className="social_link">
-          <img className="social_icon" src={github_logo} alt={github_logo} />
-        </a>
-      </nav>
       <ThemeContext.Consumer>
         {({ theme, setTheme }) => (
           <Toggle
