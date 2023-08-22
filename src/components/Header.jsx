3c00536e9e8e6461a567749f9f6b2784afe6d368
= <=>
2023-08-22 11:22:36 +0300

    redo resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,24 @@ import Toggle from "../components/ThemeToggler";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div>
+      <header className="header">
+        <div className="header__wrapper">
+          <h1 className="header__title">
+            <strong>
+              Hi, my name is <em>Alex</em>
+            </strong>
+            <br />a frontend developer
+          </h1>
+          <div className="header__text">
+            <p>with passion for learning and creating.</p>
+          </div>
+          <a href="#!" className="btn">
+            Download CV
+          </a>
+        </div>
+      </header>
+
       <ThemeContext.Consumer>
         {({ theme, setTheme }) => (
           <Toggle
