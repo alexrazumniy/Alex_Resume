3788ad444eba73d3083e9274b78ca39a1f71086f
= <=>
2023-08-27 15:28:05 +0300

    projects added

--- src/helpers/projectsList.jsx
+++ src/helpers/projectsList.jsx
@@ -6,46 +6,46 @@ import img05 from "./../assets/projects/05.jpg";
 import img06 from "./../assets/projects/06.jpg";
 
 const projectslist = [
-    {
-        id: 1,
-        title: "Internet Shop",
-        skills: "React, Node.js, MongoDB",
-        img: img01,
-        gitHubLink: "https://alexrazumniy.github.io/MakeITtastier/"
-    },
-    {
-        id: 2,
-        title: "Task Manager",
-        skills: "React, Node.js, MongoDB",
-        img: img02,
-        gitHubLink: "https://alexrazumniy.github.io/Task-Manager/"
-    },
-    {
-        id: 3,
-        title: "Landing Page",
-        skills: "React, Node.js, MongoDB",
-        img: img03,
-        gitHubLink: "https://alexrazumniy.github.io/Task-Manager/"
-    },
-    {
-        id: 4,
-        title: "Pic service",
-        skills: "React, Node.js, MongoDB",
-        img: img04,
-        gitHubLink: "https://alexrazumniy.github.io/Task-Manager/"
-    },
-    {
-        id: 5,
-        title: "Apple service",
-        skills: "React, Node.js, MongoDB",
-        img: img05,
-    },
-    {
-        id: 6,
-        title: "Android service",
-        skills: "React, Node.js, MongoDB",
-        img: img06,
-    },
-]
+  {
+    id: 1,
+    title: "Landing Page",
+    skills: "HTML, CSS",
+    img: img01,
+    gitHubLink: "https://alexrazumniy.github.io/project-foundation/",
+  },
+  {
+    id: 2,
+    title: "Landing Page",
+    skills: "HTML, CSS, jQuery",
+    img: img02,
+    gitHubLink: "https://alexrazumniy.github.io/project-landguru/",
+  },
+  {
+    id: 3,
+    title: "Web Application",
+    skills: "HTML, CSS, Javascript",
+    img: img03,
+    gitHubLink: "https://alexrazumniy.github.io/Task-Manager/",
+  },
+  {
+    id: 4,
+    title: "Food order service",
+    skills: "React, Node.js",
+    img: img04,
+    gitHubLink: "https://alexrazumniy.github.io/MakeITtastier/",
+  },
+  {
+    id: 5,
+    title: "Apple service",
+    skills: "React, Node.js, MongoDB",
+    img: img05,
+  },
+  {
+    id: 6,
+    title: "Android service",
+    skills: "React, Node.js, MongoDB",
+    img: img06,
+  },
+];
 
-export {projectslist}
+export { projectslist };
